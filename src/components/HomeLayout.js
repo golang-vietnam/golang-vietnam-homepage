29bed38741aab7076e1a30bbf7262c329baf94f9
zlatan <zlatan.pham@gmail.com>
2019-01-30 17:30:34 +0700

    clear out mess

--- src/components/HomeLayout.js
+++ src/components/HomeLayout.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { StaticQuery, graphql } from 'gatsby'
-import { ThemeProvider } from 'styled-components'
 import Root from '@/components/root'
 import Header from '@/components/Header'
+import Footer from '@/components/Footer'
 import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
@@ -12,25 +12,13 @@ const GlobalStyle = createGlobalStyle`
     background-color: ${props => props.theme.main.body};
   }
 `
-
 const HomeLayout = ({ children }) => (
   <Root>
     <GlobalStyle />
     <Header absolute dark />
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `0px 1.0875rem 1.45rem`,
-        paddingTop: 0,
-      }}
-    >
+    <div>
       {children}
-      <footer>
-        © {new Date().getFullYear()}, Built with
-        {` `}
-        <a href="https://www.gatsbyjs.org">Gatsby</a>
-      </footer>
+      <Footer />
     </div>
   </Root>
 )

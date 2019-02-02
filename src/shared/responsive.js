e8f65588a1a4f703dfdfb8831f2e93185b21742d
zlatan <zlatan.pham@gmail.com>
2019-02-02 17:49:10 +0700

    add responsive func

---
+++ src/shared/responsive.js
@@ -0,0 +1,11 @@
+export const lg = str => {
+  return `@media (min-width: 992px){${str}}`
+}
+
+export const md = str => {
+  return `@media (min-width: 768px){${str}}`
+}
+
+export const sm = str => {
+  return `@media (min-width: 576px){${str}}`
+}

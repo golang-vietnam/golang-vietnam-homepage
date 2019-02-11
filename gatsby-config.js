a1b3c2821afc1c0a79dcfff916b7498d53195926
zlatan <zlatan.pham@gmail.com>
2019-02-11 09:28:56 +0700

    update theme color

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,8 +24,8 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#3BA2BC`,
+        theme_color: `#3BA2BC`,
         display: `minimal-ui`,
         icon: `src/images/gopher-icon.png`, // This path is relative to the root of the site.
       },

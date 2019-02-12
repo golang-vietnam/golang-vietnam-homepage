43ad8df588b40edc27d67f581d6cf64b30c19bcd
zlatan <zlatan.pham@gmail.com>
2019-02-12 18:13:09 +0700

    connect data source for article

--- src/pages/resources/articles/index.js
+++ src/pages/resources/articles/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import { graphql, StaticQuery } from 'gatsby'
 
 import DefaultLayout from '@/components/DefaultLayout'
 import SEO from '@/components/seo'
@@ -7,22 +8,49 @@ import Heading from '@/components/Heading'
 import EventCard from '@/components/EventCard'
 import { SubHeading, Card, CardHeading, CardExcerpt } from '@/shared/styled'
 
+const query = graphql`
+  {
+    data: allMarkdownRemark(
+      filter: { frontmatter: { key: { eq: "articles" } } }
+    ) {
+      edges {
+        node {
+          id
+          frontmatter {
+            list {
+              title
+              desc
+              linkURL
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
 const ArticlesPage = () => (
   <DefaultLayout title="Resources">
     <SEO title="Articles" />
 
     <SubHeading className="mb-8">Articles</SubHeading>
 
-    {Array.from({ length: 10 }, (v, i) => (
-      <Card key={i}>
-        <CardHeading>Effective Go</CardHeading>
-        <CardExcerpt>
-          Go is a new language. Although it borrows ideas from existing
-          languages, it has unusual properties that make effective Go programs
-          different in character from programs written in its relatives.
-        </CardExcerpt>
-      </Card>
-    ))}
+    <StaticQuery
+      query={query}
+      render={({ data }) => {
+        if (!data) {
+          return null
+        }
+        const { list } = data.edges[0].node.frontmatter
+
+        return list.map(({ title, desc, linkURL }, i) => (
+          <Card key={i}>
+            <CardHeading>{title}</CardHeading>
+            <CardExcerpt>{desc}</CardExcerpt>
+          </Card>
+        ))
+      }}
+    />
   </DefaultLayout>
 )
 

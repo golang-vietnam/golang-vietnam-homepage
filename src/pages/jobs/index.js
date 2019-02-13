ffcd64f7c06a6d5ff6a8331e9ff245839805d8f4
zlatan <zlatan.pham@gmail.com>
2019-02-13 09:56:27 +0700

    bind job to data source

--- src/pages/jobs/index.js
+++ src/pages/jobs/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import { graphql, StaticQuery } from 'gatsby'
 
 import DefaultLayout from '@/components/DefaultLayout'
 import SEO from '@/components/seo'
@@ -7,13 +8,45 @@ import Heading from '@/components/Heading'
 import data from '@/data/jobs'
 import JobCard from '@/components/JobCard'
 
+const query = graphql`
+  {
+    data: allMarkdownRemark(filter: { frontmatter: { key: { eq: "jobs" } } }) {
+      edges {
+        node {
+          id
+          frontmatter {
+            list {
+              company
+              date
+              desc
+              isOpened
+              linkURL
+              location
+              title
+              type
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
 const JobPage = () => (
   <DefaultLayout title="Jobs">
     <SEO title="Jobs" />
 
-    {data.map(news => (
-      <JobCard {...news} key={news.id} />
-    ))}
+    <StaticQuery
+      query={query}
+      render={({ data }) => {
+        if (!data) {
+          return null
+        }
+        const { list } = data.edges[0].node.frontmatter
+
+        return list.map((data, i) => <JobCard {...data} key={i} />)
+      }}
+    />
   </DefaultLayout>
 )
 

ebf4d46ad3ed05ed1690f3cd689b8aae0ffad909
zlatan <zlatan.pham@gmail.com>
2019-04-14 20:43:55 +0700

    update 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,49 @@
 import React from 'react'
+import { Link } from 'gatsby'
+import DefaultLayout from '@/components/DefaultLayout'
+import NotFoundIllustration from '@/components/svg/NotFoundIllustration'
 
-import HomeLayout from '../components/HomeLayout'
 import SEO from '../components/seo'
 
 const NotFoundPage = () => (
-  <HomeLayout>
+  <DefaultLayout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </HomeLayout>
+    <div
+      className="w-full text-center flex flex-col justify-center items-center"
+      css={`
+        min-height: calc(100vh - 480px);
+      `}
+    >
+      <div
+        css={`
+          width: 100%;
+          max-width: 456px;
+        `}
+      >
+        <NotFoundIllustration />
+      </div>
+      <h1
+        className="mt-8"
+        css={`
+          font-size: 40px;
+        `}
+      >
+        404 Page Not Found
+      </h1>
+      <p
+        css={`
+          margin-top: 10px;
+          a {
+            font-weight: 600;
+            text-decoration: underline;
+            color: ${props => props.theme.main.primary};
+          }
+        `}
+      >
+        Better go back to safe lands, like the <Link to="/">Start Page</Link>
+      </p>
+    </div>
+  </DefaultLayout>
 )
 
 export default NotFoundPage

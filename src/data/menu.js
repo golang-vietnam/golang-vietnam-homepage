131b02582ac90a86ef25b4f6d1b139a3911105c1
zlatan <zlatan.pham@gmail.com>
2020-05-22 22:30:05 +0700

    feat: add gopher conf link on header

--- src/data/menu.js
+++ src/data/menu.js
@@ -12,25 +12,21 @@ export default [
     href: '/#events'
   },
   {
-    name: 'Resources',
-    href: '/resources'
-    // children: [
-    //   {
-    //     name: 'Videos',
-    //     href: '/resources/videos',
-    //   },
-    //   {
-    //     name: 'Articles',
-    //     href: '/resources/articles',
-    //   },
-    //   {
-    //     name: 'Books',
-    //     href: '/resources/books',
-    //   },
-    // ],
-  },
-  {
     name: 'Members',
     href: '/members'
+  },
+  {
+    name: 'GopherCon',
+    href: '#',
+    children: [
+      {
+        name: 'Gohphercon 2019',
+        href: 'https://gophercon.vn/'
+      },
+      {
+        name: 'Gohphercon 2018',
+        href: 'https://2018.gophercon.vn/'
+      }
+    ]
   }
 ];

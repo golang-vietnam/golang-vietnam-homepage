062b0757b8d69b63d9fe2e354ea6bf4405018c2e
zlatan <zlatan.pham@gmail.com>
2021-01-04 15:36:09 +0700

    chore: remove netlify cms dependency

--- src/cms/cms.js
+++
@@ -1,6 +0,0 @@
-// import CMS from 'netlify-cms';
-
-// Register preview template here
-
-// import FooterPreview from './preview-templates/FooterPreview'
-// CMS.registerPreviewTemplate('footer', FooterPreview)
